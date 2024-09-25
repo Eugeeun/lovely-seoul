e69162bf48448ab83b3b52da68eef381890e0375
Eugeeun <jack603@naver.com>
2024-09-25 17:24:04 +0900

    ✨ 장소 마커 기능 추가

--- src/components/KakaoMap/KakaoMap.jsx
+++ src/components/KakaoMap/KakaoMap.jsx
@@ -1,28 +1,45 @@
 import { useState } from 'react';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import styles from './KakaoMap.module.scss';
-import zoomInImage from '/zoom-in.svg'; // 줌 인 이미지 경로
-import zoomOutImage from '/zoom-out.svg'; // 줌 아웃 이미지 경로
+import zoomInImage from '/zoom-in.svg';
+import zoomOutImage from '/zoom-out.svg';
 
 const centerOfSeoul = {
   lat: 37.5665,
   lng: 126.978,
 };
 
-const KakaoMap = props => {
+const markerImage = {
+  src: '/place-pin.png',
+  size: { width: 24, height: 35 },
+  options: {
+    offset: { x: 12, y: 35 },
+  },
+};
+
+const KakaoMap = ({ placeLists }) => {
   const [level, setLevel] = useState(8);
 
   const zoomIn = () => {
-    setLevel(prevLevel => Math.max(prevLevel - 1, 1)); // 최소 레벨 1
+    setLevel(prevLevel => Math.max(prevLevel - 1, 1));
   };
 
   const zoomOut = () => {
-    setLevel(prevLevel => Math.min(prevLevel + 1, 14)); // 최대 레벨 14
+    setLevel(prevLevel => Math.min(prevLevel + 1, 14));
   };
 
   return (
     <div>
-      <Map center={centerOfSeoul} level={level} className={styles.kakoMap} />
+      <Map center={centerOfSeoul} level={level} className={styles.kakoMap}>
+        {placeLists.map((marker, i) => (
+          <MapMarker
+            key={i}
+            position={{ lat: marker.x, lng: marker.y }}
+            title={marker.area_nm}
+            image={markerImage}
+          />
+        ))}
+      </Map>
       <div className={styles.buttonContainer}>
         <img src={zoomInImage} alt='줌 인' onClick={zoomIn} />
         <img src={zoomOutImage} alt='줌 아웃' onClick={zoomOut} />
