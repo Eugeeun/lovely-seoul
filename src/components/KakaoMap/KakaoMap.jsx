42440d0c921b5d0b7ebc8a9bdb06d234b304f133
Eugeeun <jack603@naver.com>
2024-09-26 14:55:06 +0900

    ✨ 문화행사 팝업 기능 업데이트

--- src/components/KakaoMap/KakaoMap.jsx
+++ src/components/KakaoMap/KakaoMap.jsx
@@ -6,6 +6,8 @@ import zoomOutImage from '/zoom-out.svg';
 import PropTypes from 'prop-types';
 import useStore from '../../store';
 import Popup from '../Popup/Popup';
+import ky from 'ky';
+import PopupEvent from '../Popup/PopupEvent';
 
 const centerOfSeoul = {
   lat: 37.5665,
@@ -14,16 +16,20 @@ const centerOfSeoul = {
 
 const markerImage = {
   src: '/place-pin.png',
-  size: { width: 24, height: 35 },
-  options: {
-    offset: { x: 12, y: 35 },
-  },
+  size: { width: 28, height: 35 },
+};
+
+const markerImage2 = {
+  src: '/event-pin.png',
+  size: { width: 28, height: 35 },
 };
 
 const KakaoMap = ({ placeLists }) => {
-  const [level, setLevel] = useState(8);
-  const { selectedMarker, setSelectedMarker, clearSelectedMarker } = useStore();
+  const [level, setLevel] = useState(6);
+  const { selectedPlace, setSelectedPlace, clearSelectedPlace } = useStore();
   const [center, setCenter] = useState(centerOfSeoul);
+  const { placeDetailInfo, setPlaceDetailInfo } = useStore();
+  const { selectedEvent, setSelectedEvent, clearSelectedEvent } = useStore();
 
   const zoomIn = () => {
     setLevel(prevLevel => Math.max(prevLevel - 1, 1));
@@ -33,32 +39,73 @@ const KakaoMap = ({ placeLists }) => {
     setLevel(prevLevel => Math.min(prevLevel + 1, 14));
   };
 
-  const handleMarkerClick = marker => {
-    setSelectedMarker(marker);
+  const fetchEventLists = async marker => {
+    const apiUrl = `http://openapi.seoul.go.kr:8088/${
+      import.meta.env.VITE_CITY_DATA_API_KEY
+    }/json/citydata/1/5/${marker.area_nm}`;
+    try {
+      const data = await ky.get(apiUrl).json();
+      setPlaceDetailInfo(data['CITYDATA']);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handlePlaceMarkerClick = marker => {
+    clearSelectedEvent();
+    setSelectedPlace(marker);
     setLevel(2);
     setCenter({ lat: marker.x, lng: marker.y });
+    fetchEventLists(marker);
   };
 
   const handleOverlayClose = () => {
-    clearSelectedMarker();
+    clearSelectedPlace();
+  };
+
+  const handleEventMarkerClick = marker => {
+    setSelectedEvent(marker);
+  };
+
+  const handleMapClick = e => {
+    if (selectedEvent) {
+      clearSelectedEvent();
+    }
   };
 
   return (
     <div>
-      <Map center={center} level={level} className={styles.kakoMap}>
+      <Map center={center} level={level} className={styles.kakoMap} onClick={handleMapClick}>
         {placeLists.map((marker, i) => (
           <MapMarker
             key={i}
             position={{ lat: marker.x, lng: marker.y }}
             title={marker.area_nm}
             image={markerImage}
-            onClick={() => handleMarkerClick(marker)}
+            onClick={() => handlePlaceMarkerClick(marker)}
           />
         ))}
 
-        {selectedMarker && (
-          <CustomOverlayMap position={{ lat: selectedMarker.x, lng: selectedMarker.y }}>
-            <Popup title={selectedMarker.area_nm} handleClose={handleOverlayClose} />
+        {placeDetailInfo &&
+          placeDetailInfo['EVENT_STTS'].map((marker, i) => (
+            <MapMarker
+              key={i}
+              position={{ lat: marker.EVENT_Y, lng: marker.EVENT_X }}
+              title={marker.EVENT_NM}
+              image={markerImage2}
+              onClick={() => handleEventMarkerClick(marker)}
+            />
+          ))}
+
+        {selectedPlace && (
+          <CustomOverlayMap position={{ lat: selectedPlace.x, lng: selectedPlace.y }}>
+            <Popup title={selectedPlace.area_nm} handleClose={handleOverlayClose} />
+          </CustomOverlayMap>
+        )}
+
+        {selectedEvent && (
+          <CustomOverlayMap position={{ lat: selectedEvent.EVENT_Y, lng: selectedEvent.EVENT_X }}>
+            <PopupEvent info={selectedEvent} />
           </CustomOverlayMap>
         )}
       </Map>
@@ -73,11 +120,11 @@ const KakaoMap = ({ placeLists }) => {
 KakaoMap.propTypes = {
   placeLists: PropTypes.arrayOf(
     PropTypes.shape({
-      x: PropTypes.string.isRequired, // 위도
-      y: PropTypes.string.isRequired, // 경도
-      area_nm: PropTypes.string.isRequired, // 지역명
+      x: PropTypes.string.isRequired,
+      y: PropTypes.string.isRequired,
+      area_nm: PropTypes.string.isRequired,
     })
-  ).isRequired, // placeLists는 필수 props
+  ).isRequired,
 };
 
 export default KakaoMap;
