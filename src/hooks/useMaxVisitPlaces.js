38f5cb7b2330a74ca35a6b6c0933a554ac713b1f
Eugeeun <jack603@naver.com>
2024-09-30 11:07:52 +0900

    📝 전체 장소 전역상태로 업데이트

--- src/hooks/useMaxVisitPlaces.js
+++ src/hooks/useMaxVisitPlaces.js
@@ -3,13 +3,14 @@ import { detailPlaceLists } from '../utils/detailPlaceLists';
 import useStore from '../store';
 
 const useMaxVisitPlaces = placeLists => {
-  const { setHotPlaceLists } = useStore();
+  const { setAllPlaceLists, setHotPlaceLists } = useStore();
 
   useEffect(() => {
     if (placeLists && placeLists.row) {
       const geocoder = new window.kakao.maps.services.Geocoder();
 
       detailPlaceLists(placeLists, geocoder).then(updatedLists => {
+        setAllPlaceLists(updatedLists);
         const maxVisitPlaces = Array.from({ length: 6 }, (_, i) => {
           const ageGroupIndex = i + 1; // 1부터 6까지의 인덱스 사용
           return updatedLists.reduce(
