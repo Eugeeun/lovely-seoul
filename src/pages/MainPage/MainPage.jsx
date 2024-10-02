1d2baf4ba5c1f79421b96ec53451c2ed2e31aa94
Eugeeun <jack603@naver.com>
2024-10-02 12:25:13 +0900

    ♻️ 메인 페이지 리팩토링

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,17 +1,17 @@
 import styles from '../common.module.scss';
-import useStore from '../../store';
 import DetailListCard from '../../components/ListCard/DetailListCard';
+import useSortedHotPlaceLists from '../../hooks/useSortedHotPlaceLists';
 
 const MainPage = () => {
-  const { hotPlaceLists } = useStore();
+  const sortedHotPlaceLists = useSortedHotPlaceLists();
 
   return (
     <ul className={styles.cardLists}>
-      {hotPlaceLists.map((place, index) => (
+      {sortedHotPlaceLists.map((place, index) => (
         <div key={index}>
           <p className={styles.ageGroup}>
-            <strong className={`${styles[`age${index + 1}0`]}`}>{index + 1}0대</strong>가 가장 많이
-            방문했어요!
+            <strong className={`${styles[`age${place.index}0`]}`}>{place.index}0대</strong>가 가장
+            많이 방문했어요!
           </p>
           <DetailListCard place={place} defaultOpen={false} />
         </div>
