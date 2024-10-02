952da2b4599948af88b6d36d0f596b33cf9aa9d0
Eugeeun <jack603@naver.com>
2024-10-02 12:24:58 +0900

    ✨ 연령별 핫플 훅 추가

---
+++ src/hooks/useSortedHotPlaceLists.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import useStore from '../store';
+
+const useSortedHotPlaceLists = () => {
+  const { hotPlaceLists, savedUserInfo } = useStore();
+  const [sortedHotPlaceLists, setSortedHotPlaceLists] = useState([]);
+
+  useEffect(() => {
+    setSortedHotPlaceLists(hotPlaceLists);
+  }, [hotPlaceLists]);
+
+  useEffect(() => {
+    const ageGroupIndex = savedUserInfo?.age ? Math.floor(savedUserInfo.age / 10) : null;
+    const newSortedHotPlaceLists = [...hotPlaceLists];
+
+    if (ageGroupIndex !== null && ageGroupIndex < newSortedHotPlaceLists.length) {
+      const [targetPlace] = newSortedHotPlaceLists.splice(ageGroupIndex - 1, 1);
+      newSortedHotPlaceLists.unshift(targetPlace);
+    }
+
+    setSortedHotPlaceLists(newSortedHotPlaceLists);
+  }, [savedUserInfo, hotPlaceLists]);
+
+  return sortedHotPlaceLists;
+};
+
+export default useSortedHotPlaceLists;
