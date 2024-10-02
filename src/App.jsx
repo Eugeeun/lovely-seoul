ee19ccb0926df8bb218f3bb79a7ef22c0199450f
Eugeeun <jack603@naver.com>
2024-10-02 15:08:24 +0900

    📝 정적에서 실시간 데이터로 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import KakaoMap from './components/KakaoMap/KakaoMap';
 import useFetch from './hooks/useFetch';
 import Loading from './components/Loading/Loading';
 import useStore from './store';
-import data from '../data.json';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import styles from './App.module.scss';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
@@ -13,36 +12,20 @@ import MyPage from './pages/MyPage/MyPage';
 import DetailPage from './pages/DetailPage/DetailPage';
 import MainPage from './pages/MainPage/MainPage';
 import SearchPage from './pages/SearchPage/SearchPage';
+import useMaxVisitPlaces from './hooks/useMaxVisitPlaces';
 
 const getAllPlaceListUrl =
   'https://data.seoul.go.kr/SeoulRtd/getCategoryList?page=1&category=%EC%A0%84%EC%B2%B4%EB%B3%B4%EA%B8%B0&count=all&sort=true';
 
 function App() {
   const { data: placeLists, error, isLoading } = useFetch(getAllPlaceListUrl);
-  const { setAllPlaceLists, setHotPlaceLists } = useStore();
   const { isLoginModalOpen, setIsLoginModalOpen } = useStore();
 
   useEffect(() => {
     setIsLoginModalOpen(!localStorage.getItem('userInfo'));
   }, []);
 
-  useEffect(() => {
-    if (placeLists && placeLists.row) {
-      setAllPlaceLists(data);
-      const maxVisitPlaces = Array.from({ length: 6 }, (_, i) => {
-        const ageGroupIndex = i + 1;
-        return data.reduce(
-          (maxPlace, place) => {
-            const popRate = parseFloat(place.population[`PPLTN_RATE_${ageGroupIndex}0`]);
-            return popRate > maxPlace.popRate ? { place, popRate } : maxPlace;
-          },
-          { popRate: 0 }
-        ).place;
-      }).map((maxVisitPlace, index) => ({ ...maxVisitPlace, index: index + 1 }));
-
-      setHotPlaceLists(maxVisitPlaces);
-    }
-  }, [placeLists, setHotPlaceLists]);
+  useMaxVisitPlaces(placeLists);
 
   if (isLoading) return <Loading loading={true} />;
   if (error) return <div>Error: {error.message}</div>;
