08790eca17e0f4d6d4e99658ef59149cfe35c986
Eugeeun <jack603@naver.com>
2024-10-02 09:47:08 +0900

    ♻️ BarGraph 컴포넌트 리팩토링

--- src/components/BarGraph/BarGraph.jsx
+++ src/components/BarGraph/BarGraph.jsx
@@ -3,49 +3,36 @@ import { colorMapping } from './color';
 import styles from './BarGraph.module.scss';
 import PropTypes from 'prop-types';
 
-const BarGraph = ({ data }) => {
-  return (
-    <div className={styles.barGraphCon}>
-      <div className={styles.barGraph}>
-        <ResponsiveBar
-          data={data}
-          keys={['10대', '20대', '30대', '40대', '50대', '60대']}
-          indexBy='label'
-          layout='horizontal' // 가로 방향
-          colors={({ id }) => colorMapping[id]} // 색상 매핑
-          axisBottom={{
-            tickValues: [],
-          }}
-        />
-      </div>
+const ageGroups = ['10대', '20대', '30대', '40대', '50대', '60대'];
 
-      <div className={styles.colorCon}>
-        {Object.keys(colorMapping).map(key => (
-          <div key={key} className={styles.colorInfo}>
-            <div
-              className={styles.colorBox}
-              style={{
-                backgroundColor: colorMapping[key],
-              }}
-            />
-            <span>{key}</span>
-          </div>
-        ))}
-      </div>
+const BarGraph = ({ data }) => (
+  <div className={styles.barGraphCon}>
+    <div className={styles.barGraph}>
+      <ResponsiveBar
+        data={data}
+        keys={ageGroups}
+        indexBy='label'
+        layout='horizontal'
+        colors={({ id }) => colorMapping[id]}
+        axisBottom={{ tickValues: [] }}
+      />
     </div>
-  );
-};
+    <div className={styles.colorCon}>
+      {ageGroups.map(age => (
+        <div key={age} className={styles.colorInfo}>
+          <div className={styles.colorBox} style={{ backgroundColor: colorMapping[age] }} />
+          <span>{age}</span>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 BarGraph.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({
       label: PropTypes.string.isRequired,
-      '10대': PropTypes.number.isRequired,
-      '20대': PropTypes.number.isRequired,
-      '30대': PropTypes.number.isRequired,
-      '40대': PropTypes.number.isRequired,
-      '50대': PropTypes.number.isRequired,
-      '60대': PropTypes.number.isRequired,
+      ...Object.fromEntries(ageGroups.map(age => [age, PropTypes.number.isRequired])),
     })
   ).isRequired,
 };
