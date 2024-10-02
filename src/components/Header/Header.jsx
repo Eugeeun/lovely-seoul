40ce7c56679b08b3408334535f122634afd25cfd
Eugeeun <jack603@naver.com>
2024-10-02 09:49:54 +0900

    🐛 빈칸 검색이 안되게 디버그

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,6 +19,7 @@ const Header = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
+    if (searchTerm.length < 1) return;
     setSavedSearchTerm(searchTerm);
     navigate('/searchpage');
     setSearchTerm('');
